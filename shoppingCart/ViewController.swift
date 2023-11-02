e3f9c7dca9605a143ac3e4a19549e0d2bc23756d
Alvin <thisiseasytoremember1224@gmail.com>
2023-11-02 09:11:20 -0500

    Initial Commit

---
+++ shoppingCart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  shoppingCart
+//
+//  Created by ALVIN CHEN on 11/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
