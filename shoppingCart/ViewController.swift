a116a553bb935c0b4e0b3916a364154459f9bcb8
Alvin <thisiseasytoremember1224@gmail.com>
2023-11-02 09:35:33 -0500

    k

--- shoppingCart/ViewController.swift
+++ shoppingCart/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")!
+        cell.textLabel?.text = list[indexPath.row]
+        return cell
+    }
+    
+//blah
+    
+    @IBOutlet weak var textFieldOutlet: UITextField!
+    
+    @IBOutlet weak var tableViewOutlet: UITableView!
+    
+    var list = ["Item", "Item2", "Item3"]
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableViewOutlet.delegate = self
+        tableViewOutlet.dataSource = self
+        
     }
 
+    
+    @IBAction func addAction(_ sender: UIButton) {
+        
+        
+        tableViewOutlet.reloadData()
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
 
